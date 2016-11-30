5c581e4ae3228ae478cb031913b787fd0bff2a0a
ravuthz <ravuthz@gmail.com>
2016-11-30 18:47:38 +0700

    Generate Controller AuthController

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+class AuthController extends \Phalcon\Mvc\Controller {
+    public function loginAction() {
+        // render view "auth/login"
+    }
+
+    public function postLoginAction() {
+        // handle login event
+    }
+
+    public function registerAction() {
+        // render view "auth/register"
+    }
+
+    public function postRegisterAction() {
+        // handle register event
+    }
+
+    public function logoutAction() {
+        // handle logout event
+    }
+}
